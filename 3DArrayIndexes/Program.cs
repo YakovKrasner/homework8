20c56475b141c94eb2d2bc4ab0ad967e837ee8c3
Yakov Krasner <yakov.krasner@yandex.ru>
2022-09-07 22:42:32 +0700

    Initial commit_3D array with indexes

---
+++ 3DArrayIndexes/Program.cs
@@ -0,0 +1,29 @@
+﻿int FirstDimension = 2;
+int SecondDimension = 2;
+int ThirdDimension = 2; 
+int lowerLimitOfRandom = 0;
+int upperLimitOfRandom = 9;
+int [,,] Array = new int [FirstDimension,SecondDimension,ThirdDimension];
+
+
+for (int i = 0; i < Array.GetLength (0); i ++)
+    {    
+        for (int j = 0; j < Array.GetLength (1); j ++)
+            {
+                for (int k = 0; k < Array.GetLength (2); k ++)
+                Array [i,j,k] = new Random().Next(lowerLimitOfRandom,upperLimitOfRandom+1);
+                
+            }
+        Console.WriteLine ();
+    }
+
+Console.WriteLine ();
+
+for (int i = 0; i < Array.GetLength (0); i ++)
+    {    
+        for (int j = 0; j < Array.GetLength (1); j ++)
+            {
+                for (int k = 0; k < Array.GetLength (2); k ++)
+                Console.WriteLine ($"{Array[i,j,k]}({i},{j},{k})");              
+            }
+    }
